eddea152058f223912bde188c36475caaeaabab7
EtienneStn <etienne.santran1@gmail.com>
2022-01-07 19:20:05 +0100

    v0.20

--- popup/popup.js
+++ popup/popup.js
@@ -1,89 +1,46 @@
-const allCheckbox = document.querySelectorAll(".tgl");
-console.log(allCheckbox);
-for (i = 0; i < allCheckbox.length; i++) {
-  allCheckbox[i].addEventListener("click", checked)
-}
-function checked(){
-  /* let selectedlist=[]; */
-
-  for(let i=0; i<allCheckbox.length; i++)       
-  {
-      if(allCheckbox[i].value === "courir" && allCheckbox[i].checked==true)
+window.onload = function () {
+  chrome.storage.local.get('checkboxOff');
+  chrome.storage.local.get('checked', function () {
+    const allCheckbox = document.querySelectorAll(".tgl");
+    console.log(allCheckbox);
+    for (i = 0; i < allCheckbox.length; i++) {
+      allCheckbox[i].addEventListener("click", checked)
+    }
+    function checked(){
+      /* let selectedlist=[]; */
+    
+      for(let i=0; i<allCheckbox.length; i++)       
       {
-        console.log("Script Courir")
+        if(allCheckbox[i].value === "courir" && allCheckbox[i].checked==true)
+        {
+          let courirCheck = document.querySelector('').checked==true
+          chrome.storage.local.set({'checked': courirCheck}, function() {
+            console.log("Script Courir");
+          });
+        }
+        else if(allCheckbox[i].value === "zalando" && allCheckbox[i].checked==true)
+        {
+          console.log("Script zalando")
+        }
+        else if(allCheckbox[i].value === "galerielafayette" && allCheckbox[i].checked==true)
+        {
+          console.log("Script galerielafayette")
+        }
+        else if(allCheckbox[i].value === "test" && allCheckbox[i].checked==true)
+        {
+          console.log("Script test")
+        }
+        else {
+          let checboxOff = allCheckbox[i].checked==false
+          chrome.storage.local.set({'checkboxOff': checboxOff}, function() {
+            console.log("Script OFF : " + allCheckbox[i].value)
+          });
+        }
       }
-      else if(allCheckbox[i].value === "zalando" && allCheckbox[i].checked==true)
-      {
-        console.log("Script zalando")
-      }
-      else if(allCheckbox[i].value === "galerielafayette" && allCheckbox[i].checked==true)
-      {
-        console.log("Script galerielafayette")
-      }
-      else if(allCheckbox[i].value === "test" && allCheckbox[i].checked==true)
-      {
-        console.log("Script test")
-      }
-      else{
-        console.log("Script OFF : " + allCheckbox[i].value)
-      }
+      
       /* selectedlist.push(allCheckbox[i].value); */
-  }
-}
-  /* let checkedSite = selectedlist;
-  console.log(checkedSite)
-  if((checkedSite))
-  {
-    if(checkedSite.find(element => element === "courir" ))
-    {
-      console.log("Script Courir")
     }
-
-    if(checkedSite.find(element => element === "zalando"))
-    {
-      console.log("Script zalando")
-    }
-
-    if(checkedSite.find(element => element === "galerielafayette"))
-    {
-      console.log("Script galerielafayette")
-    }
-
-    if(checkedSite.find(element => element === "test"))
-    {
-      console.log("Script Courir")
-    }
-  }
-  else
-  {
-    console.log("Scrip OFF")
-  }
-} */
-
-/* function storageCheck(e) {
-  const defaultValue = false;
-  chrome.storage.sync.get(selectedlist);
-  console.log(selectedlist)
-}
- */
-/* function storeSwitch() {
-  var switchState = document.getElementById(allCheckbox).checked;
-  chrome.storage.sync.set({activeSwitch: switchState}, function () {
-    console.log('activeSwitch saved');
   });
+  let getUrl = window.location
+  console.log(getUrl)
 }
-
-document.getElementById(allCheckbox).addEventListener('change', storeSwitch); */
-/* <div>
-	<img src="logo128.png"/>
-
-	<label class="switch">
-
-		<input type="checkbox" id="activeSwitch"/>
-
-		<span class="slider round"></span>
-
-	</label>
-	
-	<script src="popup.js"></script>
-</div> */
--- script/content_script.js
+++ script/content_script.js
@@ -1,26 +1,62 @@
-const size = document.querySelector('.swatches').children;
-const sizeRange = size.length;
-const inStock = document.querySelectorAll('.selectable');
-const oss = document.querySelectorAll('.unselectable');
-const sizeWanted = "35,5";
-console.log(size);
-console.log(inStock);
-console.log(oss);
-let inStockFormated = sizeRange.className === "selectable"
-for(let i=0;i < size.length; i++){
-    if(inStockFormated)
-    {
-        let sizeSplit = size[i];
-        let sizeNumber = size[i].textContent;
-        let sizeFormated = sizeNumber.replace(/ /g, "");
-        console.log(sizeSplit)
-        console.log(sizeFormated)
-    }
+const timeAtc = 2222;
+const getSnkrs = async function () {
+    let pick = await randomPick();
+    let cart = await atc();
+    let ship = await redirect('https://www.courir.com/fr/cart');
 }
-let randSize = inStockFormated[Math.floor(Math.random() * inStockFormated.length)];
-console.log("RandSize : ", randSize)
-/* const sizeInstock = Array.from(inStock);
-const sizeOSS = Array.from(oss); 
-const sizeRange = sizeInstock.concat(sizeOSS) */
+let randomPick = async function () {
+    const inStock = document.querySelectorAll('.selectable');
+    const oss = document.querySelectorAll('.unselectable');
+    let randSize = Math.floor(Math.random() * inStock.length);
+    let randValue = inStock[randSize];
+    console.log("RandSize : ", randValue);
+    let sizeSelected = randValue.childNodes[1];
+    console.log(sizeSelected);
+    setTimeout(
+        sizeSelected.click(), timeAtc
+    );
+}
+let atc = async function () {
+    const ATCButton = document.querySelector('#add-to-cart');
+    setTimeout(
+        ATCButton.click(), timeAtc
+    );
+}
+let get = function (url) {
+    var req = new XMLHttpRequest()
+    req.open('GET', url, true);
+}
+let redirect = async function (url) {
+    setTimeout(
+        window.location.href = url, timeAtc
+    );
+}
+
+getSnkrs();
+//let getUrl = window.location
+//console.log(getUrl)
+//let url = get('file:///D:/Code/Coto/CotoFnF/Script/popup/cotoscript-popup.html');
+//let button = url.querySelector('.tgl-button')
+//console.log(button)
 
 
+
+//    const size = document.querySelector('.swatches').children;
+//    const sizeRange = size.length;
+//    for(let i=0;i < size.length; i++){
+//     if(size[i].className === "selectable")
+//     {
+//         let array = [];
+//         let sizeSelectable = size[i];
+//         let arraySelectable = array.push(sizeSelectable)
+//         console.log(array)
+//         /* let sizeSplit = size[i];
+//         let sizeNumber = size[i].textContent;
+//         let sizeFormated = sizeNumber.replace(/ /g, "");
+//         console.log(sizeSplit)
+//         console.log(sizeFormated) */
+//     }
+// }
+// const sizeInstock = Array.from(inStock);
+// const sizeOSS = Array.from(oss); 
+// const sizeRange = sizeInstock.concat(sizeOSS)
